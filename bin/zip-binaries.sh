cebe5fe63ceea31ec7d309ad70bd5b3794ac9e8d
Amanda Baran <adb321lehigh.edu>
2026-05-25 12:31:47 -0400

    fix zip binary

--- bin/zip-binaries.sh
+++ bin/zip-binaries.sh
@@ -6,4 +6,7 @@ SCRIPT_DIR="$( realpath -sm  "$( dirname "${BASH_SOURCE[0]}" )")"
 cd "$SCRIPT_DIR"
 
 rm -rf bin.zip
-zip -Dj bin.zip swarm-kv/{swarm-kv,fusee}/build/bin/*
+zip -Dj bin.zip chimera/chimera/build/bin/* chimera/{swarm-kv,fusee}/build/bin/*
+
+echo "Success: bin/bin.zip created with swarmkv, raw_memory, chimera, and fusee."
+
